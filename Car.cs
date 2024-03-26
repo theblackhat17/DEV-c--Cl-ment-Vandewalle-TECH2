a81b2cf79fc2f3f1afee90a32b0dfcacaa6a258f
theblackhat17 <69344806+theblackhat17@users.noreply.github.com>
2024-03-26 12:57:47 +0100

    Ajout de la fonctionnalité d'Affichage des "NON"

--- Car.cs
+++ Car.cs
@@ -7,8 +7,8 @@ public class Car
     public string Marque { get; private set; }
     public int Annee { get; private set; }
     public Colors Couleur { get; set; }
-
     private Queue<Tache> checklist = new Queue<Tache>();
+    private List<string> reponsesNon = new List<string>(); 
 
     public string NomComplet => $"{Marque} {Modele}";
 
@@ -23,34 +23,41 @@ public class Car
         checklist.Enqueue(new Tache("Essence remplie"));
         checklist.Enqueue(new Tache("Ceinture mise"));
     }
+
     public void AskForChecklist()
     {
         Console.WriteLine("Veuillez compléter la checklist :");
         while (checklist.Count > 0)
         {
-            Tache currentTask = checklist.Peek(); 
+            Tache currentTask = checklist.Peek();
             Console.WriteLine($"Avez-vous {currentTask.Nom}? (oui/non)");
-
             string reponse = Console.ReadLine().Trim().ToLower();
+
             if (reponse == "oui")
             {
-                checklist.Dequeue(); 
-                currentTask.IsDone = true;
+                checklist.Dequeue();
+            }
+            else
+            {
+                reponsesNon.Add(currentTask.Nom); 
             }
         }
 
+        if (reponsesNon.Count > 0)
+        {
+            Console.WriteLine("Récapitulatif des tâches pour lesquelles la réponse était 'non' avant la validation finale :");
+            foreach (string tache in reponsesNon)
+            {
+                Console.WriteLine($"- {tache}");
+            }
+            Console.WriteLine("Toutefois, toutes les tâches ont finalement été validées.");
+        }
+        
         Console.WriteLine("Vroum Vroum, la voiture démarre !");
     }
 
     public void StartEngine()
     {
-        if (checklist.Count == 0)
-        {
-            Console.WriteLine("Le moteur démarre : Vroum Vroum!");
-        }
-        else
-        {
-            Console.WriteLine("La checklist n'est pas complète. La voiture ne peut pas démarrer.");
-        }
+        Console.WriteLine("Le moteur démarre : Vroum Vroum!");
     }
 }
